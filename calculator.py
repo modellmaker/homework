aeaf737f0adbd11889875383d42f2fd593dce79f
Dávid Balázs <modellmaker01@gmail.com>
2020-10-19 13:02:19 +0200

    homework 8.3

--- calculator.py
+++ calculator.py
@@ -1,7 +1,27 @@
 print("Hi, I'm Your BASIC calculator")
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = input("Enter the first number: ")
+while True:
+    try:
+        num1 = int(num1)
+    except ValueError:
+        print(num1, "not a number.")
+        num1 = input("Enter the first number: ")
+    else:
+        break
+
+num2 = input("Enter the second number: ")
+
+while True:
+    try:
+        num2 = int(num2)
+    except ValueError:
+        print(num2, "not a number.")
+        num2 = input("Enter the second number: ")
+    else:
+        break
+
+
 operation = input("Enter the operand: ")
 
 if operation == "+":
